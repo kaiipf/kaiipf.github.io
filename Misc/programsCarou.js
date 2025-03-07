4995a9f2ca846d736e09d54e723f1b707b7ccd00
kaiipf <khoo.kj1@gmail.com>
2025-03-07 20:51:37 +0800

    Fixed the responsive program carousel and modified some stuff

--- Misc/programsCarou.js
+++ Misc/programsCarou.js
@@ -1,8 +1,8 @@
-var numDots = 5;
-var numItems = 6;
-var currentSlide = 0;
-var currentTimeout = null;
-var consideredClick = false;
+var noOfDots = 5
+var noOfItems = 6
+var currentSlide = 0
+var currentTimeout = null
+var consideredClick = false
 var slideFiles = [
     'Images/visualArtPoster.jpeg',
     'Images/storyBrdPoster.jpeg',
@@ -10,99 +10,93 @@ var slideFiles = [
     'Images/steamPoster.jpeg',
     'Images/digArtPoster.jpeg',
     'Images/fineArtPoster.jpeg',
-];
-
-function limitNumberWithinRange(num, min, max) {
+]
+function limitNumberWithinRange(num, min, max){
     const MIN = min ?? 0;
-    const MAX = max ?? numDots;
-    const parsed = parseInt(num);
-    return Math.min(Math.max(parsed, MIN), MAX);
+    const MAX = max ?? noOfDots;
+    const parsed = parseInt(num)
+    return Math.min(Math.max(parsed, MIN), MAX)
 }
-
 function changeSlide(slide) {
-    timeout();
-    document.getElementById("carousel" + limitNumberWithinRange(currentSlide)).classList.remove("selectedCarousel");
-    document.getElementById("carousel" + limitNumberWithinRange(currentSlide)).classList.add("unselectedCarousel");
-    currentSlide = limitNumberWithinRange(slide);
-    document.getElementById("carouselContainer").style.left = "-" + (currentSlide * 302) + "px";
-    document.getElementById("carousel" + currentSlide).classList.remove("unselectedCarousel");
-    document.getElementById("carousel" + currentSlide).classList.add("selectedCarousel");
-    console.log((currentSlide * 302));
+    timeout()
+    document.getElementById("carou" + limitNumberWithinRange(currentSlide)).classList.remove("selectedCarousel")
+    document.getElementById("carou" + limitNumberWithinRange(currentSlide)).classList.add("unselectedCarousel")
+    currentSlide = limitNumberWithinRange(slide)
+    document.getElementById("partnerCarousel").style.left = "-" + (currentSlide * 302) + "px"
+    document.getElementById("carou" + currentSlide).classList.remove("unselectedCarousel")
+    document.getElementById("carou" + currentSlide).classList.add("selectedCarousel")
+    console.log((currentSlide * 302))
 }
-
 function next(slide) {
     if (slide == "back") {
         if (currentSlide - 1 <= -1) {
-            changeSlide(numDots);
+            changeSlide(noOfDots)
         } else {
-            changeSlide(currentSlide - 1);
+            changeSlide(currentSlide - 1)
         }
     } else if (slide == "front") {
-        if (currentSlide + 1 > numDots) {
-            changeSlide(0);
+        if (currentSlide + 1 > noOfDots) {
+            changeSlide(0)
         } else {
-            changeSlide(currentSlide + 1);
+            changeSlide(currentSlide + 1)
         }
-    } else {
-        changeSlide(slide);
+        } else {
+        changeSlide(slide)
     }
 }
-
 function timeout() {
-    clearTimeout(currentTimeout);
-    currentTimeout = setTimeout(function() {
-        next("front");
-        timeout();
-    }, 5000);
-}
-timeout();
+        clearTimeout(currentTimeout)
+        currentTimeout = setTimeout(function() {
+                next("front")
+            timeout();
+        }, 5000);
+    }
+    timeout()
 
-for (let i = 0; i < numDots + 1; i++) {
-    document.getElementById("dotWrapper").innerHTML = document.getElementById("dotWrapper").innerHTML + '<button class="unselectedCarousel" style="width:10px; height:10px; border-radius:80px; margin:1px; cursor:pointer;" onclick="next(' + i + ')" id="carousel' + i + '"></button>';
+for (let i = 0; i < noOfDots+1; i++) { 
+    document.getElementById("dotContainer").innerHTML = document.getElementById("dotContainer").innerHTML + '<button class="unselectedCarousel" style="width:10px; height:10px; border-radius:80px; margin:1px; cursor:pointer;" onclick="next('+ i +')"" id="carou'+ i +'"></button>'
 }
-
-changeSlide(currentSlide);
+changeSlide(currentSlide)
 
 var touchUpdate;
 let xPos;
 let curTouch;
-var touchStarted = false;
-
+var touchStarted = false
 function touchStart(event) {
-    clearTimeout(currentTimeout);
-    consideredClick = true;
-    console.log(event.touches[0].pageX);
-    touchStarted = true;
-    xPos = event.touches[0].pageX;
-    curTouch = (currentSlide * 302);
-    setTimeout(function() {
-        consideredClick = false;
-    }, 50);
+clearTimeout(currentTimeout)
+consideredClick = true
+console.log(event.touches[0].pageX)
+touchStarted = true
+xPos = event.touches[0].pageX;
+curTouch = (currentSlide * 302)
+setTimeout(function() {
+    consideredClick = false
+}, 50);
 }
-
 function touchMove(event) {
     if (touchStarted == true) {
         event.preventDefault();
+
         if (event.touches[0].clientX < xPos) {
-            console.log(event.touches[0].clientX - xPos);
-            curTouch = event.touches[0].clientX * 0.5;
-            document.getElementById("carouselContainer").style.left = -((currentSlide * 302) + (xPos - event.touches[0].clientX * 0.5)) + "px";
+            console.log(event.touches[0].clientX - xPos)
+            curTouch = event.touches[0].clientX * 0.5
+            document.getElementById("partnerCarousel").style.left = - ((currentSlide * 302)  + (xPos - event.touches[0].clientX * 0.5)) + "px"
         } else {
-            // you swiped right
-            console.log(event.touches[0].clientX - xPos);
-            curTouch = event.touches[0].clientX;
-            document.getElementById("carouselContainer").style.left = -((currentSlide * 302) + (xPos - event.touches[0].clientX)) + "px";
+        // you swiped right
+            console.log(event.touches[0].clientX - xPos)
+            curTouch = event.touches[0].clientX 
+            document.getElementById("partnerCarousel").style.left = - ((currentSlide * 302) + (xPos - event.touches[0].clientX)) + "px"
         }
     }
+    
 }
-
 function touchEnd(event) {
     if (touchStarted == true && consideredClick == false) {
-        touchStarted = false;
-        console.log(Math.round((currentSlide * 302) + (xPos - curTouch)) / 302);
-        changeSlide(Math.round(((currentSlide * 302) + (xPos - curTouch)) / 302));
+        touchStarted = false
+        console.log(Math.round((currentSlide * 302) + (xPos - curTouch))/ 302)
+        changeSlide(Math.round(((currentSlide * 302) + (xPos - curTouch))/ 302));
     } else if (consideredClick == true) {
-        openPoster(slideFiles[currentSlide]);
+        openPoster(slideFiles[currentSlide])
     }
-    consideredClick = false;
-}
+    consideredClick = false
+};
---
+++ Misc/programsCarou2.js
@@ -0,0 +1,104 @@
+var programnoOfDots = 5
+var programnoOfItems = 6
+var programcurrentSlide = 0
+var programcurrentTimeout = null
+var consideredClick = false
+
+var slideFiles = [
+    'Images/visualArtPoster.jpeg',
+    'Images/storyBrdPoster.jpeg',
+    'Images/clayPoster.jpeg',
+    'Images/steamPoster.jpeg',
+    'Images/digArtPoster.jpeg',
+    'Images/fineArtPoster.jpeg',
+]
+
+function programlimitNumberWithinRange(num, min, max){
+    const MIN = min ?? 0;
+    const MAX = max ?? programnoOfDots;
+    const parsed = parseInt(num)
+    return Math.min(Math.max(parsed, MIN), MAX)
+}
+function programchangeSlide(slide) {
+    programtimeout()
+    document.getElementById("programCarou" + programlimitNumberWithinRange(programcurrentSlide)).classList.remove("selectedCarousel")
+    document.getElementById("programCarou" + programlimitNumberWithinRange(programcurrentSlide)).classList.add("unselectedCarousel")
+    programcurrentSlide = programlimitNumberWithinRange(slide)
+    document.getElementById("programCarousel").style.left = "-" + (programcurrentSlide * 302) + "px"
+    document.getElementById("programCarou" + programcurrentSlide).classList.remove("unselectedCarousel")
+    document.getElementById("programCarou" + programcurrentSlide).classList.add("selectedCarousel")
+    console.log((programcurrentSlide * 302))
+}
+function programnext(slide) {
+    if (slide == "back") {
+        if (programcurrentSlide - 1 <= -1) {
+            programchangeSlide(programnoOfDots)
+        } else {
+            programchangeSlide(programcurrentSlide - 1)
+        }
+    } else if (slide == "front") {
+        if (programcurrentSlide + 1 > programnoOfDots) {
+            programchangeSlide(0)
+        } else {
+            programchangeSlide(programcurrentSlide + 1)
+        }
+        } else {
+            programchangeSlide(slide)
+    }
+}
+function programtimeout() {
+        clearTimeout(programcurrentTimeout)
+        programcurrentTimeout = setTimeout(function() {
+                programnext("front")
+                programtimeout();
+        }, 5000);
+    }
+    programtimeout()
+
+for (let i = 0; i < programnoOfDots+1; i++) { 
+    document.getElementById("programContainer").innerHTML = document.getElementById("programContainer").innerHTML + '<button class="unselectedCarousel" style="width:10px; height:10px; border-radius:80px; margin:1px; cursor:pointer;" onclick="programnext('+ i +')"" id="programCarou'+ i +'"></button>'
+}
+programchangeSlide(0)
+
+var programtouchUpdate;
+let programxPos;
+let programcurTouch;
+
+var programtouchStarted = false
+const programtouchStart = (event) => {
+clearTimeout(programcurrentTimeout)
+console.log("program")
+programtouchStarted = true
+programxPos = event.touches[0].pageX;
+programcurTouch = (programcurrentSlide * 302)
+setTimeout(function() {
+    consideredClick = false
+}, 50);
+}
+
+function programtouchMove(event) {
+    if (programtouchStarted == true) {
+        event.preventDefault();
+
+        if (event.touches[0].clientX < programxPos) {
+            console.log(event.touches[0].clientX - programxPos)
+            programcurTouch = event.touches[0].clientX * 0.5
+            document.getElementById("programCarousel").style.left = - ((programcurrentSlide * 302)  + (programxPos - event.touches[0].clientX * 0.5)) + "px"
+        } else {
+        // you swiped right
+            console.log(event.touches[0].clientX - programxPos)
+            programcurTouch = event.touches[0].clientX 
+            document.getElementById("programCarousel").style.left = - ((programcurrentSlide * 302) + (programxPos - event.touches[0].clientX)) + "px"
+        }
+    }
+}
+const programtouchEnd = (event) => {
+    if (programtouchStarted == true && consideredClick == false) {
+        programtouchStarted = false
+        console.log(Math.round((programcurrentSlide * 302) + (programxPos - programcurTouch))/ 302)
+        programchangeSlide(Math.round(((programcurrentSlide * 302) + (programxPos - programcurTouch))/ 302));
+    } else if (consideredClick == true) {
+        openPoster(slideFiles[currentSlide])
+    }
+    consideredClick = false
+};
